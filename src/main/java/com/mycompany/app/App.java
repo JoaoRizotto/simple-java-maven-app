3fba28a33cc6476be73a4faaf5c7e2282b5e4bbc
JoaoRizotto <39994784+JoaoRizotto@users.noreply.github.com>
2020-12-12 14:49:34 -0300

    java-calculadora-7
    Ajuste de bugs e funções

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,21 +1,46 @@
 package com.mycompany.app;
+import java.util.*;
+import java.util.Scanner;
 
 /**
- * Hello world!
+ * Projeto Final da matéria de Laboratório de Engenharia de Software
  */
 public class App
 {
-
-    private final String message = "Hello World!";
-
+    /*Declara Appteste*/
     public App() {}
-
     public static void main(String[] args) {
-        System.out.println(new App().getMessage());
+		
+		int num1= 10;
+		int num2= 5;
+		
+		System.out.println(soma(num1,num2));
+		System.out.println(sub(num1,num2));
+		System.out.println(multi(num1,num2));
+		System.out.println(divi(num1,num2));
+		
     }
-
-    private final String getMessage() {
-        return message;
+    
+    /*declara funções*/
+    public static int soma(int a, int b)
+    {
+        int res = a + b;
+        return res;
     }
-
+    
+    public static int sub(int a, int b)
+    {
+        int res = a - b;
+        return res;
+    }
+    public static int multi(int a, int b)
+    {
+        int res = a * b;
+        return res;
+    }
+    public static int divi(int a, int b)
+    {
+        int result = a / b;
+        return res;
+    }
 }
